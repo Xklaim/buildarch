bd7269b97346aa58b2918e16d020f333d8861558
xklaim <iampunitbhardwaj@gmail.com>
2024-09-16 13:34:20 +0530

    added modal for info

--- js/script.js
+++ js/script.js
@@ -205,3 +205,33 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
+
+// modal js
+
+// Get the modal
+const modal = document.getElementById('infoModal');
+
+// Get the button that opens the modal
+const modalBtn = document.getElementById('infoBtn');
+
+// Get the <span> element that closes the modal
+const modalCloseBtn = document.querySelector('.close');
+
+// When the user clicks the button, open the modal
+modalBtn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+modalCloseBtn.onclick = function() {
+  modal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
